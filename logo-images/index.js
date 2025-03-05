f441a958062e64b4c783c535fdce4e91bffb96d8
AngelAyonV <angelayon815@gmail.com>
2025-03-04 20:42:01 -0700

    Updates

---
+++ logo-images/index.js
@@ -0,0 +1,17 @@
+const images = {
+    react: "/logo-images/react-image.png",
+    javascript: "/logo-images/js-image.png",
+    rubyRails: "/logo-images/rubyrails-image.png",
+    html: "/logo-images/html-image.png",
+    css: "/logo-images/css-image.png",
+    tailwind: "/logo-images/tailwind-image.png",
+    reactNative: "/logo-images/reactnative-image.png",
+    bootstrap: "/logo-images/boostrap-image.png",
+    php: "/logo-images/php-image.png",
+    cpp: "/logo-images/c++-image.png",
+    sql: "/logo-images/sql-image.png",
+    mysql: "/logo-images/mysql-image.png",
+    postgresql: "/logo-images/postgresql-image.png",
+  };
+  
+  export default images;
